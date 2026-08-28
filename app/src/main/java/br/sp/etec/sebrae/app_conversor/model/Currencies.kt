78d83fd39af04a45a31879b61203979158a03467
Douglas Galdino <douglasgaldino701@gmail.com>
2026-08-28 09:13:51 -0300

    Melhoria no backend e finalização do app

--- app/src/main/java/br/sp/etec/sebrae/app_conversor/model/Currencies.kt
+++ app/src/main/java/br/sp/etec/sebrae/app_conversor/model/Currencies.kt
@@ -2,5 +2,8 @@ package br.sp.etec.sebrae.app_conversor.model
 
 data class Currencies(
     val USD : Moeda,
-    val EUR : Moeda
+    val EUR : Moeda,
+    val GBP : Moeda,
+    val ARS : Moeda
+
 )
