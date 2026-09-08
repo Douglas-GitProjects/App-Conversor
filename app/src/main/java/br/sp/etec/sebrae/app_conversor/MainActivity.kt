7aa74462a59e4e1a7a09bf3f7f5c6015e981ae3c
Douglas <douglasgaldino701@gmail.com>
2026-09-08 11:03:32 -0300

    08/09 projeto finalizado

--- app/src/main/java/br/sp/etec/sebrae/app_conversor/MainActivity.kt
+++ app/src/main/java/br/sp/etec/sebrae/app_conversor/MainActivity.kt
@@ -2,7 +2,11 @@ package br.sp.etec.sebrae.app_conversor
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,6 +18,12 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    var cotacaoDollar : Double = 0.0
+    var cotacaoEuro : Double = 0.0
+    var cotacaoPeso : Double = 0.0
+    var cotacaoLibra : Double = 0.0
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,21 +34,49 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val moedas = arrayOf("Dólar", "Euro", "Peso Argentino", "Libra")
+        val spMoeda = findViewById<Spinner>(R.id.spMoedas)
+
+        val moedasAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, moedas)
+
+        spMoeda.adapter = moedasAdapter
+        carregarCotacoes()
+
         val btnConverte = findViewById<Button>(R.id.btnConverte)
         btnConverte.setOnClickListener {
-            ClientApi.api.getCotacoes().enqueue(object : Callback<FinanceResponse> {
-                override fun onResponse(
-                    p0: Call<FinanceResponse?>,
-                    response: Response<FinanceResponse?>
-                ) {
-                    println(response.body().toString())
+            val valor = findViewById<EditText>(R.id.txtValor).text.toString().toDouble()
+            val itemSelecionado = spMoeda.selectedItem.toString()
+            val valorCotacao = when(itemSelecionado){
+                "Dólar" -> valor * cotacaoDollar
+                "Euro" -> valor * cotacaoEuro
+                "Peso Argentino" -> valor * cotacaoPeso
+                "Libra" -> valor * cotacaoLibra
+                else -> {
+                    0.0
                 }
-
-                override fun onFailure(p0: Call<FinanceResponse?>, p1: Throwable) {
-                    TODO("Not yet implemented")
-                }
-
-            })
+            }
+            val resultado = findViewById<TextView>(R.id.textviewresultado)
+            resultado.text = "Valor em %.2f".format(valorCotacao)
         }
+    }
+
+    private fun carregarCotacoes() {
+        ClientApi.api.getCotacoes().enqueue(object : Callback<FinanceResponse> {
+            override fun onResponse(
+                p0: Call<FinanceResponse?>,
+                response: Response<FinanceResponse?>
+            ) {
+                val moedas = response.body()?.results?.currencies
+                cotacaoDollar = moedas?.USD?.buy ?: 0.0
+                cotacaoEuro = moedas?.USD?.buy ?: 0.0
+                cotacaoPeso = moedas?.USD?.buy ?: 0.0
+                cotacaoLibra = moedas?.USD?.buy ?: 0.0
+            }
+
+            override fun onFailure(p0: Call<FinanceResponse?>, p1: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
